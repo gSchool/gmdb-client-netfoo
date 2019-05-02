d1295b679212b186e20c6aba43eeaa44b843b869
sochetanie <allagorik2901@gmail.com>
2019-05-02 09:40:09 -0400

    fixed search function

--- gmdb-reviews/src/app/movie-list/movie-list.component.ts
+++ gmdb-reviews/src/app/movie-list/movie-list.component.ts
@@ -25,23 +25,22 @@ export class MovieListComponent implements OnInit, OnChanges {
   }
 
   ngOnInit() {
-    // this.update();
+    this.update();
   }
 
-  // update(){
-  //   console.log('this.actRouter.snapshot.params ' + this.actRouter.snapshot.params['query'])
-  //   this.actRouter.params.subscribe(data => this.query = data.query);
-
-  //   console.log('this.query ' + this.query)
-
-  //   if(!this.query){
-  //     console.log('This query is empty')
-  //     this.ms.getAll().subscribe(movies => this.movieList = movies);
-  //   }
-  //   else{
-  //     this.ms.getMovieByName(this.query).subscribe(movies => this.movieList = movies);
-  //     console.log('When query is not empty we get: ' + this.movieList);
-  //   }
-  // }
-
+  update(){
+    console.log('this.actRouter.snapshot.params ' + this.actRouter.snapshot.params['query'])
+    this.actRouter.params.subscribe(data => {
+      this.query = data.query;
+      console.log('This is paramater' + this.query);
+      if(this.query===''){
+        console.log('This query is empty')
+        this.ms.getAll().subscribe(movies => this.movieList = movies);
+      }
+      else{
+        this.ms.getMovieByName(this.query).subscribe(movies => this.movieList = movies);
+        console.log('When query is not empty we get: ' + this.movieList[0].Title);
+      }
+    });
+  }
 }
